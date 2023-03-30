8c8bd9adfe74e69bd934421a61a5c13385fb5e7b
dat.ngo <dat.ngo@fusionstar.tech>
2023-03-30 20:41:26 +0800

    finish carousel

--- js/carousel/index.js
+++ js/carousel/index.js
@@ -6,18 +6,51 @@ const imageShowerElems = document.querySelectorAll(
 );
 
 let activeIndex = 0;
+let running = false;
+const totalImageShower = imageShowerElems.length;
 imageShowerElems[activeIndex].classList.add('active');
 
 function moveToLeft() {
-  let nextIndex = activeIndex + 1;
+  if (running) {
+    return;
+  }
+  running = true;
+  // let nextIndex = activeIndex + 1;
+  // if (nextIndex >= totalImageShower) {
+  //   nextIndex = 0;
+  // }
+  let nextIndex = (activeIndex + 1) % totalImageShower;
   imageShowerElems[activeIndex].classList.add('active-move-to-left');
   imageShowerElems[nextIndex].classList.add('next-active-move-to-left');
   setTimeout(() => {
     imageShowerElems[activeIndex].classList.remove('active');
     imageShowerElems[activeIndex].classList.remove('active-move-to-left');
-    imageShowerElems[nextIndex].classList.add('next-active-move-to-left');
+    imageShowerElems[nextIndex].classList.remove('next-active-move-to-left');
     imageShowerElems[nextIndex].classList.add('active');
     activeIndex = nextIndex;
+    running = false;
+  }, 500);
+}
+
+function moveToRight() {
+  if (running) {
+    return;
+  }
+  running = true;
+  // let nextIndex = activeIndex - 1;
+  // if (nextIndex < 0) {
+  //   nextIndex = totalImageShower - 1;
+  // }
+  let nextIndex = (activeIndex - 1 + totalImageShower) % totalImageShower;
+  imageShowerElems[activeIndex].classList.add('active-move-to-right');
+  imageShowerElems[nextIndex].classList.add('next-active-move-to-right');
+  setTimeout(() => {
+    imageShowerElems[activeIndex].classList.remove('active');
+    imageShowerElems[activeIndex].classList.remove('active-move-to-right');
+    imageShowerElems[nextIndex].classList.remove('next-active-move-to-right');
+    imageShowerElems[nextIndex].classList.add('active');
+    activeIndex = nextIndex;
+    running = false;
   }, 500);
 }
 
@@ -26,5 +59,9 @@ arrowLeftElem.addEventListener('click', () => {
 });
 
 arrowRightElem.addEventListener('click', () => {
-  alert('Right Click');
+  moveToRight();
 });
+
+setInterval(() => {
+  moveToRight();
+}, 3000);
